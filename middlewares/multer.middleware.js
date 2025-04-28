8f287dfe57e7111dfccaa5244b42bd5b9a450620
naman <Nmndwdi1001@gmail.com>
2025-04-29 01:24:55 +0530

    uuid replaced by Date.now()

--- middlewares/multer.middleware.js
+++ middlewares/multer.middleware.js
@@ -5,7 +5,8 @@ const storage=multer.diskStorage({
         cb(null, '/tmp');
     },
     filename: function(req,file,cb) {
-        cb(null, uuidv4() + path.extname(file.originalname));
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        cb(null, uniqueSuffix + path.extname(file.originalname));
     }
 })
 
